da329f14efc21d2f6f108ff7a8034b19e5a63d79
Romain <delaplaineromain@gmail.com>
2022-05-15 13:36:35 +0200

    color change by clicking done

--- Assets/Scripts/script_planet.cs
+++ Assets/Scripts/script_planet.cs
@@ -7,7 +7,7 @@ public class script_planet : MonoBehaviour
     public Transform center_rotation;
 
     private float angle;
-    [SerializeField] private float radius = 3;
+    private float radius = 3;
     [SerializeField] private float rotation_speed = 1;
 
     private Vector3 newTranslation;
@@ -27,6 +27,31 @@ public class script_planet : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             space_key_pressed = !space_key_pressed;
+        }
+
+        // mouse click dtection
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject.name.ToLower() == "center")
+                {
+                    var rand = new System.Random();
+
+
+                    var RGBcolor1 = new byte[3];
+                    rand.NextBytes(RGBcolor1);
+
+                    var RGBcolor2 = new byte[3];
+                    rand.NextBytes(RGBcolor2);
+
+                    hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = new Color32(RGBcolor1[0], RGBcolor1[1], RGBcolor1[2], 255);
+                    GetComponent<MeshRenderer>().material.color = new Color32(RGBcolor2[0], RGBcolor2[1], RGBcolor2[2], 255);
+                }
+            }
         }
     }
 
